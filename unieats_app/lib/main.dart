aa66941351a87aaac4037120bc43d66adac1bd83
hijriah <hijriah247@gmail.com>
2025-12-19 22:50:54 +0800

    update customer homepage

--- unieats_app/lib/main.dart
+++ unieats_app/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'landing_screen.dart';
+import 'customer_homepage.dart'; // import the home screen
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -15,10 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Delivery App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LandingScreen(), // landing page first
+      title: 'Vendor App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CustomerHomepage(),
     );
   }
 }
