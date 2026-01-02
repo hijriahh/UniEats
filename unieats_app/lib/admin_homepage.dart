f247c38c43e0ef0288c435345a2e1765e1d2a78e
peesenee <preszlatak18@gmail.com>
2026-01-03 03:25:05 +0800

    logout

--- unieats_app/lib/admin_homepage.dart
+++ unieats_app/lib/admin_homepage.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'admin_dashboard_page.dart';
 import 'admin_approval_page.dart';
+import 'login_page.dart';
 
 const Color kAdminPrimary = Color(0xFFB7916E);
 const Color kBackgroundColor = Color(0xFFF6F6F6);
 
 class AdminHomepage extends StatelessWidget {
   const AdminHomepage({Key? key}) : super(key: key);
+
+  void _logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +27,7 @@ class AdminHomepage extends StatelessWidget {
       child: Scaffold(
         backgroundColor: kBackgroundColor,
         appBar: AppBar(
+          automaticallyImplyLeading: false, // ❌ remove back button
           backgroundColor: kAdminPrimary,
           elevation: 0,
           title: const Text(
@@ -22,6 +35,16 @@ class AdminHomepage extends StatelessWidget {
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
+
+          // Logout button
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              tooltip: "Logout",
+              onPressed: () => _logout(context),
+            ),
+          ],
+
           bottom: const TabBar(
             indicatorColor: Colors.white,
             tabs: [
